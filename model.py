3b9c36092cdf596cb7bc62bfd42dc9a9c259fe25
wei-lun-huang <weilun.huang@gogolook.com>
2023-08-29 01:14:53 +0800

    feat: add github actions

--- model.py
+++ model.py
@@ -2,27 +2,27 @@ import os
 import sys
 from sqlalchemy import create_engine
 
-# db_user = os.environ.get("MYSQL_USER")
-# print(db_user)
-# db_password = os.environ.get("MYSQL_PWD")
-# print(db_password)
-# db_host = os.environ.get("MYSQL_HOST")
-# print(db_host)
-# db_port = os.environ.get("MYSQL_PORT")
-# print(db_port)
-# db_name = os.environ.get("MYSQL_DB")
-# print(db_name)
-
-db_user = sys.argv[1]
+db_user = os.environ.get("MYSQL_USER")
 print(db_user)
-db_password = sys.argv[2]
+db_password = os.environ.get("MYSQL_PWD")
 print(db_password)
-db_host = sys.argv[3]
+db_host = os.environ.get("MYSQL_HOST")
 print(db_host)
-db_port = int(sys.argv[4])
+db_port = os.environ.get("MYSQL_PORT")
 print(db_port)
-db_name = sys.argv[5]
+db_name = os.environ.get("MYSQL_DB")
 print(db_name)
+
+# db_user = sys.argv[1]
+# print(db_user)
+# db_password = sys.argv[2]
+# print(db_password)
+# db_host = sys.argv[3]
+# print(db_host)
+# db_port = int(sys.argv[4])
+# print(db_port)
+# db_name = sys.argv[5]
+# print(db_name)
 
 
 db_url = (
