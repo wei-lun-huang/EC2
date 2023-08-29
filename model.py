7c907b733cab018a2b2759d8ae3c14808998eec4
wei-lun-huang <weilun.huang@gogolook.com>
2023-08-29 10:46:01 +0800

    feat: add github actions

---
+++ model.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+
+print(os.environ)
+db_user = os.environ.get("MYSQL_USER")
+print(db_user)
+db_password = os.environ.get("MYSQL_PWD")
+print(db_password)
+db_host = os.environ.get("MYSQL_HOST")
+print(db_host)
+db_port = os.environ.get("MYSQL_PORT")
+print(db_port)
+db_name = os.environ.get("MYSQL_DB")
+print(db_name)
+
+
+db_url = (
+    f"mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?charset=utf8mb4"
+)
+engine = create_engine(db_url)
+connection = engine.connect()
+print(connection)
